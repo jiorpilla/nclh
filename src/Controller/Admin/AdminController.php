bdb88029d98a38781447f8a1df6c37e0027170d1
jiorpilla <jiorpilla@gmail.com>
2023-10-27 19:01:30 +0800

    tested stimulus
    tested stimulus, and added an admin controller testing the SRC/CONTROLLER/ADM(IN namespace and routing annotations

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin', name: 'admin_')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'test')]
+    public function test()
+    {
+        return $this->render('admin/admin_test.twig', [
+        ]);
+    }
+
+}
