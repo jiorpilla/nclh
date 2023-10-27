1942f2bad95421a0f9cc66f236a9d18e79748c2c
jiorpilla <jiorpilla@gmail.com>
2023-10-27 18:27:24 +0800

    added webpack encore, jquery and stimulus

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
-class TestController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+class TestController extends AbstractController
+{
+    #[Route('/', name: 'test')]
+    public function test()
+    {
+
+        return $this->render('test.twig', [
+        ]);
+    }
 }
