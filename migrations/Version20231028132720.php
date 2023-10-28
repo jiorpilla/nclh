9837210d2347c3341b3e22d63696446d15823c14
jiorpilla <jiorpilla@gmail.com>
2023-10-28 21:34:31 +0800

    User to Users entity renaming
    change entity user to users
    also added several additional fields on users

---
+++ migrations/Version20231028132720.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231028132720 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD email VARCHAR(255) DEFAULT NULL, ADD physician_id BINARY(16) DEFAULT NULL COMMENT \'(DC2Type:ulid)\', ADD nurse_id BINARY(16) DEFAULT NULL COMMENT \'(DC2Type:ulid)\', ADD crew_id BINARY(16) DEFAULT NULL COMMENT \'(DC2Type:ulid)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP email, DROP physician_id, DROP nurse_id, DROP crew_id');
+    }
+}
--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -2,14 +2,16 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+use App\Repository\UsersRepository;
 use App\Utils\Traits\CommonEntityTrait;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UlidType;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Uid\Ulid;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+#[ORM\Entity(repositoryClass: UsersRepository::class)]
+class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
     use CommonEntityTrait;
 
@@ -24,6 +26,19 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+
+    #[ORM\Column(type: 'ulid', nullable: true)]
+    private ?Ulid $physician_id = null;
+
+    #[ORM\Column(type: 'ulid', nullable: true)]
+    private ?Ulid $nurse_id = null;
+
+    #[ORM\Column(type: 'ulid', nullable: true)]
+    private ?Ulid $crew_id = null;
 
     public function getUsername(): ?string
     {
@@ -88,5 +103,53 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhysicianId(): ?Ulid
+    {
+        return $this->physician_id;
+    }
+
+    public function setPhysicianId(?Ulid $physician_id): static
+    {
+        $this->physician_id = $physician_id;
+
+        return $this;
+    }
+
+    public function getNurseId(): ?Ulid
+    {
+        return $this->nurse_id;
+    }
+
+    public function setNurseId(?Ulid $nurse_id): static
+    {
+        $this->nurse_id = $nurse_id;
+
+        return $this;
+    }
+
+    public function getCrewId(): ?Ulid
+    {
+        return $this->crew_id;
+    }
+
+    public function setCrewId(?Ulid $crew_id): static
+    {
+        $this->crew_id = $crew_id;
+
+        return $this;
     }
 }
