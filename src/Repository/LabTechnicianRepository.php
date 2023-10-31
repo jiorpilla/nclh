f0c0d5e93207f9c3a1966bf60fb2abdb3b78b98a
jiorpilla <jiorpilla@gmail.com>
2023-10-31 17:30:45 +0800

    added lab technician entity

---
+++ src/Repository/LabTechnicianRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\LabTechnician;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<LabTechnician>
+ *
+ * @method LabTechnician|null find($id, $lockMode = null, $lockVersion = null)
+ * @method LabTechnician|null findOneBy(array $criteria, array $orderBy = null)
+ * @method LabTechnician[]    findAll()
+ * @method LabTechnician[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LabTechnicianRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, LabTechnician::class);
+    }
+
+//    /**
+//     * @return LabTechnician[] Returns an array of LabTechnician objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('l')
+//            ->andWhere('l.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('l.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?LabTechnician
+//    {
+//        return $this->createQueryBuilder('l')
+//            ->andWhere('l.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
