9d654ba97e1efaf95b79e18820f7ace56e3ee665
jiorpilla <jiorpilla@gmail.com>
2023-11-02 00:01:12 +0800

    + enroid/qr code bundle
    added enroid qr code. this will be used to produce qr code for CREW(patients)

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 ];
