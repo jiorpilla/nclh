03f1e1c050d0b0943368309fe1851f99190fb5a6
jiorpilla <jiorpilla@gmail.com>
2023-11-02 04:29:21 +0800

    + fpdf and composer ~ and + example for QR CODE
    added PDF support for qr code,
    added twig functionalities for direct download of image or pdf of qr code

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use Endroid\QrCode\Builder\BuilderInterface;
+use Endroid\QrCodeBundle\Response\QrCodeResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\NilUlid;
+use Symfony\Component\Uid\Ulid;
 
 #[Route('/admin', name: 'admin_')]
 class AdminController extends AbstractController
@@ -11,7 +16,43 @@ class AdminController extends AbstractController
     #[Route('/', name: 'test')]
     public function test()
     {
+
+        $ulid = new Ulid();
+        $ulid2 = new NilUlid(); //null
+
+        $param['toBinary'] = $ulid->toBinary();  // string(16) "\x01\x71\x06\x9d\x59\x3d\x97\xd3\x8b\x3e\x23\xd0\x6d\xe5\xb3\x08"
+        $param['toBase32'] = $ulid->toBase32();  // string(26) "01E439TP9XJZ9RPFH3T1PYBCR8"
+        $param['toBase58'] = $ulid->toBase58();  // string(22) "1BKocMc5BnrVcuq2ti4Eqm"
+        $param['toRfc4122'] = $ulid->toRfc4122(); // string(36) "0171069d-593d-97d3-8b3e-23d06de5b308"
+        $param['toHex'] = $ulid->toHex();     // string(34) "0x0171069d593d97d38b3e23d06de5b308"
+
+        echo "<pre>";
+        print_r($ulid);
+        echo "</pre>";
+
         return $this->render('admin/admin_test.twig', [
+            'id' => $ulid,
+            'id2' => $ulid2
+        ]);
+    }
+
+    #[Route('/qr' ,name: 'qr')]
+    public function qrShow(BuilderInterface $customQrCodeBuilder): Response
+    {
+//        $result = $customQrCodeBuilder
+//            ->data('000001')
+//            ->build();
+//
+//
+//        $response = new QrCodeResponse($result);
+
+
+
+//        return $response;
+        return  $this->render('admin/qr.twig', [
+//            'result' => $result,
+//            'qr' => $result->getImage(),
+//            'r_qr' => $response
         ]);
     }
 
