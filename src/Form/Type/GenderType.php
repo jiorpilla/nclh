201d3a2eee1c6274dfc411868753bb789d719fac
jiorpilla <jiorpilla@gmail.com>
2023-11-16 04:38:22 +0800

    + a gender type
    added a gender type form. basically this is a radio button.. this is a test for now. will update later if it needs to be modified

---
+++ src/Form/Type/GenderType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GenderType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'choices' => [
+                'Male' => 'male',
+                'Female' => 'female',
+            ],
+            'expanded' => true,
+            'multiple' => false,
+            'data' => 'male',
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return ChoiceType::class;
+    }
+}
