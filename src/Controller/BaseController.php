ee0724975273f5e058d5d8822e472114447f0de6
jiorpilla <jiorpilla@gmail.com>
2023-11-30 20:16:31 +0800

    added a Paginate method
    so that how i will paginate will be centralized

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\Query;
+use Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BaseController extends AbstractController
@@ -12,8 +15,24 @@ class BaseController extends AbstractController
      */
     private int $page_limit = 15;
 
+    /*
+     * $paginator -> paginator from KNP
+     */
+    public function __construct(private PaginatorInterface $paginator)
+    {
+    }
+
     protected function getPageLimit():int
     {
         return $this->page_limit;
+    }
+
+    protected function paginate(Query $query, int $page = 1):?SlidingPagination
+    {
+        return $this->paginator->paginate(
+            $query,
+            $page,
+            $this->getPageLimit(),
+        );
     }
 }
