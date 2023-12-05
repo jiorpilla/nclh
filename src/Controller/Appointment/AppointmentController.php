8071b08c3231e7f8aa5a504beb99f270dbca1d68
jiorpilla <jiorpilla@gmail.com>
2023-12-05 20:10:15 +0800

    added controller and form and the view files

---
+++ src/Controller/Appointment/AppointmentController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Appointment;
+
+use App\Controller\BaseController;
+use App\Entity\Appointment;
+use App\Form\AppointmentType;
+use App\Repository\AppointmentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/appointment', name: 'appointment_')]
+class AppointmentController extends BaseController
+{
+    #[Route('/', name: 'index')]
+    public function index(Request $request, EntityManagerInterface $entityManager, AppointmentRepository $appointmentRepository): Response
+    {
+
+        $appointment = new Appointment();
+        $form = $this->createForm(AppointmentType::class, $appointment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($appointment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('appointment_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+
+        $query = $appointmentRepository->getListQuery();
+        $page = $request->query->get('page', 1);
+
+        $appointment_lists = $this->paginate($query, $page);
+
+        return $this->render('appointment/index.html.twig', [
+            'appointment_lists' => $appointment_lists,
+            'appointment' => $appointment,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/bulk_create', name: 'bulk_create')]
+    public function bulkCreate()
+    {
+
+    }
+}
---
+++ src/Form/AppointeeType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Appointee;
+use App\Form\Type\GenderType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AppointeeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('company')
+            ->add('first_name')
+            ->add('last_name')
+            ->add('middle_name')
+            ->add('suffix')
+            ->add('gender',GenderType::class)
+            ->add('date_of_birth', BirthdayType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('location_of_birth')
+            ->add('phone_number')
+            ->add('email', EmailType::class)
+            ->add('civil_status')
+            ->add('Address', AddressType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Appointee::class,
+            'label' => false,
+        ]);
+    }
+}
