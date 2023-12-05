50c80cbf820cd04bb23c61adaa9492d9c133dd9d
jiorpilla <jiorpilla@gmail.com>
2023-12-05 17:09:37 +0800

    Update AddressType.php

--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Address;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +13,14 @@ class AddressType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description')
+            ->add('description', TextType::class, [
+                'label' => 'Address Description'
+            ])
             ->add('street')
             ->add('barangay')
             ->add('city')
             ->add('province')
-            ->add('region')
+//            ->add('region')
             ->add('postalCode')
         ;
     }
@@ -26,6 +29,7 @@ class AddressType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Address::class,
+            'label' => 'Address Fields',
         ]);
     }
 }
