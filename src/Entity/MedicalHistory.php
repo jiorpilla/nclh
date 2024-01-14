ed3a90931c13cfc3164528d86dbcb999481b064e
jiorpilla <jiorpilla@gmail.com>
2024-01-14 18:46:55 +0800

    Update MedicalHistory.php
    make the end date nullable

--- src/Entity/MedicalHistory.php
+++ src/Entity/MedicalHistory.php
@@ -19,7 +19,7 @@ class MedicalHistory
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $startDate = null;
 
-    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
     private ?\DateTimeInterface $endDate = null;
 
     #[ORM\Column(length: 255)]
