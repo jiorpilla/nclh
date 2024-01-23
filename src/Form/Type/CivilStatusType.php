41b8ee92acba3d1170c4ffd932b4b5e04ce1a16b
jiorpilla <jiorpilla@gmail.com>
2024-01-24 06:08:58 +0800

    Create CivilStatusType.php
    customized a Type for Civil Status. might change int to string later for cleaner and more readable DB

---
+++ src/Form/Type/CivilStatusType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CivilStatusType extends AbstractType
+{
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'choices' => [
+                'Single' => '1',
+                'Married' => '2',
+                'Divorced' => '3',
+                'Widowed' => '4',
+                // Add other civil status options as needed
+            ],
+            'expanded' => true,
+            'multiple' => false,
+            'data' => '1',
+            'label_attr' => ['class' => 'radio-inline'],
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return ChoiceType::class;
+    }
+}
