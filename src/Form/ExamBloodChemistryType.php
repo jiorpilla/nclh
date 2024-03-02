eac18e051e7639d9e838b5f764d46e54fb043055
jiorpilla <jiorpilla@gmail.com>
2024-03-02 15:46:19 +0800

    Update ExamBloodChemistryType.php
    make this field text to cater the value that will be inputted by lab tech

--- src/Form/ExamBloodChemistryType.php
+++ src/Form/ExamBloodChemistryType.php
@@ -9,6 +9,7 @@ use App\Entity\MedicalHistory;
 use App\Form\Type\ExamFieldStatusType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,14 +19,14 @@ class ExamBloodChemistryType extends AbstractType
     {
         $row_attr = ['class' => 'mb-3 col-md-6'];
         $builder
-            ->add('glucose', ExamFieldStatusType::class, options: ['row_attr' => $row_attr])
-            ->add('bun', ExamFieldStatusType::class, options: ['row_attr' => $row_attr])
-            ->add('creatinine', ExamFieldStatusType::class, options: ['row_attr' => $row_attr])
-            ->add('totalBilirubin', ExamFieldStatusType::class, options: ['row_attr' => $row_attr])
-            ->add('alt', ExamFieldStatusType::class, options: ['row_attr' => $row_attr])
-            ->add('ast', ExamFieldStatusType::class, options: ['row_attr' => $row_attr])
-            ->add('totalCholesterol', ExamFieldStatusType::class, options: ['row_attr' => $row_attr])
-            ->add('triglycerides', ExamFieldStatusType::class, options: ['row_attr' => $row_attr])
+            ->add('glucose', TextType::class, options: ['row_attr' => $row_attr])
+            ->add('bun', TextType::class, options: ['row_attr' => $row_attr])
+            ->add('creatinine', TextType::class, options: ['row_attr' => $row_attr])
+            ->add('totalBilirubin', TextType::class, options: ['row_attr' => $row_attr])
+            ->add('alt', TextType::class, options: ['row_attr' => $row_attr])
+            ->add('ast', TextType::class, options: ['row_attr' => $row_attr])
+            ->add('totalCholesterol', TextType::class, options: ['row_attr' => $row_attr])
+            ->add('triglycerides', TextType::class, options: ['row_attr' => $row_attr])
         ;
     }
 
