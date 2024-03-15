2542b17ba23acfa4fc0912b973d4e9ec905bfcd3
Querwin <querwin@gaconnector.com>
2024-03-16 01:16:04 +0800

    added new design improvements

--- assets/controllers/crew_edit_controller.js
+++ assets/controllers/crew_edit_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
     static targets = ['editButton','updateButton','formGroup']
 
     formEditable(){
-        var formFields = this.formGroupTarget.querySelectorAll('input[type="text"]');
+        var formFields = this.formGroupTarget.querySelectorAll('input, select');
         for (var i = 0; i < formFields.length; i++) {
             formFields[i].toggleAttribute('disabled');
         }
